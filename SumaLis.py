a99cc58a5aa5da45cb58a178c7596f0628d9819c
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2024-12-04 18:43:38 -0600

    Add files via upload
    Se hara la suma entre listas

---
+++ SumaLis.py
@@ -0,0 +1,28 @@
+# Retornar la suma de los elementos impares de una lista.
+ 
+import random
+ 
+def sumar_impares_lista(lista):
+    suma = 0
+    for num in lista:
+        if num % 2 != 0:
+            suma += num
+ 
+    return suma
+ 
+def imprimir_lista(lista, nombre):
+    for i, num in enumerate(lista):
+        print(f"{nombre}[{i}] = {num}")
+ 
+def leer_lista():
+    lista = []
+ 
+    i = 0
+    while i < 5:
+        lista.append(random.randint(0, 5))
+        i += 1
+    return lista
+ 
+A = leer_lista()
+imprimir_lista(A, "A")
+print("Suma =", sumar_impares_lista(A))
