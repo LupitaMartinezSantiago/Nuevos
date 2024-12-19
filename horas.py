eb16c033518f70bc705a6cbeda8c88ddd063c10b
LupitaMartinezSantiago <167836035+LupitaMartinezSantiago@users.noreply.github.com>
2024-12-18 22:59:15 -0600

    Update horas.py

--- horas.py
+++ horas.py
@@ -1,6 +1,6 @@
 
 horas_trabajadas = float(input("Ingrese las horas trabajadas: "))
 tarifa_por_hora = float(input("Ingrese la tarifa por hora: "))
- 
+ # Se imprimira el salario por las horas trabajadas
 salario = horas_trabajadas * tarifa_por_hora
 print(f"El salario es: ${salario}")
